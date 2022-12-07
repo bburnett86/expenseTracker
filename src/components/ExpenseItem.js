cf11ef01301fc81cdb690998959bb1822d1c3307
Breton Burnett <breton.burnett@gmail.com>
2022-12-07 17:34:11 -0600

    Initialize app with ExpenseItem javacrtipt portion of component complete

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,19 @@
+function ExpenseItem () {
+  return (
+    <div>
+      <div>
+        March 28th, 2021
+      </div>
+      <div>
+        <h2>
+          Car Insruance
+        </h2>
+        <div>
+          249.67
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ExpenseItem
